e46a5e105e54277374aecff4cd64405415c90c6b
alfatcse <alfat.cse.21@gmail.com>
2023-12-17 22:40:37 +0100

    [Done] lonin functionality

--- src/pages/Signin/Signin.js
+++ src/pages/Signin/Signin.js
@@ -1,28 +1,50 @@
-import React from 'react';
-
-const Signin = () => {
-    const handleRegister = (e) => {
-        e.preventDefault();
-        const data = {
-            email: e.target.email.value,
-            password: e.target.password.value,
-        }
-        console.log("data: ", data)
+import { gql, useMutation } from "@apollo/client";
+import React, { useEffect, useState } from "react";
+const LOGIN = gql`
+  mutation Login($email: String!, $password: String!) {
+    signIn(email: $email, password: $password) {
+      token
     }
-
-    return (
-        <div className="form">
-            <form onSubmit={handleRegister}>
-
-                <label htmlFor="">Your Email</label>
-                <input name="email" type="email" />
-                <label htmlFor="">Your Password</label>
-                <input name="password" type="password" />
-
-                <button className='rounded-full p-2 bg-white text-black'>Login</button>
-            </form>
-        </div>
-    );
+  }
+`;
+const Signin = () => {
+  const [Login, { data, error, loading }] = useMutation(LOGIN);
+  const handleRegister = (e) => {
+    e.preventDefault();
+    const data = {
+      email: e.target.email.value,
+      password: e.target.password.value,
+    };
+    console.log("data: ", data);
+    Login({
+      variables: data,
+    });
+  };
+  const [userError, setError] = useState(null);
+  useEffect(() => {
+    if (data && data.signIn.token) {
+      console.log("token", data.signIn.token);
+      localStorage.setItem("token", data.signIn.token);
+    }
+    if (data && data.signIn?.userError) {
+      setError(data.signIn.userError);
+    }
+  }, [data]);
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error:{error.message}</p>;
+  return (
+    <div className="form">
+      <form onSubmit={handleRegister}>
+        <label htmlFor="">Your Email</label>
+        <input name="email" type="email" />
+        <label htmlFor="">Your Password</label>
+        <input name="password" type="password" />
+        <button type="submit" className="rounded-full p-2 bg-white text-black">
+          Login
+        </button>
+      </form>
+    </div>
+  );
 };
 
 export default Signin;
--- src/pages/Signup/Signup.js
+++ src/pages/Signup/Signup.js
@@ -1,31 +1,60 @@
-import React from 'react';
-import './Signup.css'
-const Signup = () => {
-    const handleRegister = (e) => {
-        e.preventDefault();
-        const data = {
-            name: e.target.name.value,
-            email: e.target.email.value,
-            password: e.target.password.value,
-            bio: e.target.bio.value
-        }
-        console.log("data: ", data)
+import React, { useEffect, useState } from "react";
+import "./Signup.css";
+import { gql, useMutation } from "@apollo/client";
+const SIGNUP = gql`
+  mutation Reg(
+    $name: String!
+    $email: String!
+    $password: String!
+    $bio: String
+  ) {
+    signup(name: $name, email: $email, password: $password, bio: $bio) {
+      userError
+      token
     }
-    return (
-        <div className="form">
-            <form onSubmit={handleRegister}>
-                <label htmlFor="">Your Name</label>
-                <input name="name" type="text" />
-                <label htmlFor="">Your Email</label>
-                <input name="email" type="email" />
-                <label htmlFor="">Your Password</label>
-                <input name="password" type="password" />
-                <label htmlFor="">Your Bio</label>
-                <input name="bio" type="text" />
-                <button className='rounded-full p-2 bg-white text-black'>Register</button>
-            </form>
-        </div>
-    );
+  }
+`;
+const Signup = () => {
+  const [signup, { data, loading, error }] = useMutation(SIGNUP);
+  const handleRegister = (e) => {
+    e.preventDefault();
+    const data = {
+      name: e.target.name.value,
+      email: e.target.email.value,
+      password: e.target.password.value,
+      bio: e.target.bio.value,
+    };
+    signup({ variables: data });
+  };
+  const [userError, setError] = useState(null);
+  useEffect(() => {
+    if (data && data.signup.token) {
+      console.log("token", data.signup.token);
+      localStorage.setItem("token", data.signup.token);
+    }
+    if (data && data.signup.userError) {
+      setError(data.signup.userError);
+    }
+  }, [data]);
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error:{error.message}</p>;
+  return (
+    <div className="form">
+      <form onSubmit={handleRegister}>
+        <label htmlFor="">Your Name</label>
+        <input name="name" type="text" />
+        <label htmlFor="">Your Email</label>
+        <input name="email" type="email" />
+        <label htmlFor="">Your Password</label>
+        <input name="password" type="password" />
+        <label htmlFor="">Your Bio</label>
+        <input name="bio" type="text" />
+        <button type="submit" className="rounded-full p-2 bg-white text-black">
+          Register
+        </button>
+      </form>
+    </div>
+  );
 };
 
 export default Signup;
